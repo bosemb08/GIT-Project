5bf492e38680916f72f767bbf3a696a8cdea69c6
bosemb08 <bosemb08@gmail.com>
2019-09-20 10:02:31 +0530

    factorial number

---
+++ src/org/test/Day3.java
@@ -0,0 +1,11 @@
+package org.test;
+public class Day3 {
+		public static void main(String[] args) {
+		int count=1;
+		for(int i =1;i<=5;i++)
+		{
+			count=count*i;
+		}
+		System.out.println(count);
+			}
+}
